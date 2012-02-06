851b3ff5c2f0079fba7dba466d32e9843b675ff0
Ryan Connelly <ryan.connelly@gmail.com>
2012-02-05 21:28:49 -0500

    changed file upload max

--- wp-config.php
+++ wp-config.php
@@ -88,3 +88,6 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+/ * Set Memory limit for file uplaods */
+define('WP_MEMORY_LIMIT', '64M');
